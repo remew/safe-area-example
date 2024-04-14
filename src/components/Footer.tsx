961a7213f49ae61390b134f84543a580c5be2fc7
remew <foolishremew@gmail.com>
2024-04-14 16:45:17 +0900

    add bottom safe area padding

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,8 @@
+import styles from "./Footer.module.css";
+
 export const Footer = () => {
   return (
-    <footer className="bg-gray-700 text-white">
+    <footer className={`bg-gray-700 text-white ${styles.root}`}>
       <div className="p-4">footer</div>
       <p className="text-center text-xs">&copy; remew</p>
     </footer>
