bbad21e920657349a9d4652ff049ce80477ee957
remew <foolishremew@gmail.com>
2024-06-06 02:43:54 +0900

    add padding for left and right

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
+import styles from "./page.module.css";
+
 export default function Home() {
   return (
-    <main className="flex-1">
+    <main className={`flex-1 ${styles.content}`}>
       <div className="h-[1000px]">main</div>
     </main>
   );
